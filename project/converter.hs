1fb2c18759edaa524a1ddd47487faa28cd29e4c9
Cameron Himes <cameron.himes@hotmail.com>
2021-11-20 18:26:59 -0500

    let VS Code fix the formatting

--- project/converter.hs
+++ project/converter.hs
@@ -9,26 +9,33 @@ import System.Environment
 	Data Type Definitions
 -}
 type Text = String
-data Inline = Normal Text       -- normal text
-            | Bold Text         -- bold text
-            | Italic Text       -- italic text
-            | BoldItalic Text   -- bold and italic text
-            | Preformatted Text -- inline code
-data Block = LI [Block]         -- List Items
-           | UL [Block]         -- Unordered List
-           | OL [Block]         -- Ordered List
-           | Heading1 [Inline]  -- Heading 1
-           | Heading2 [Inline]  -- Heading 2
-           | Paragraph [Inline] -- Paragraph
-           | Code [Inline]      -- Code block
+
+data Inline
+  = Normal Text -- normal text
+  | Bold Text -- bold text
+  | Italic Text -- italic text
+  | BoldItalic Text -- bold and italic text
+  | Preformatted Text -- inline code
+
+data Block
+  = LI [Block] -- List Items
+  | UL [Block] -- Unordered List
+  | OL [Block] -- Ordered List
+  | Heading1 [Inline] -- Heading 1
+  | Heading2 [Inline] -- Heading 2
+  | Paragraph [Inline] -- Paragraph
+  | Code [Inline] -- Code block
+
 type Document = [Block]
-data Token = Star               -- used for bold/italic
-           | Dash               -- used for unordered lists
-           | BackTick           -- used for code/preformatted text
-           | NewLine            -- used to shorten lines or end a block
-           | B Block            -- preparsed Block type
-           | I Inline           -- preparsed Inline type
-           | T Text             -- preparsed Text type
+
+data Token
+  = Star -- used for bold/italic
+  | Dash -- used for unordered lists
+  | BackTick -- used for code/preformatted text
+  | NewLine -- used to shorten lines or end a block
+  | B Block -- preparsed Block type
+  | I Inline -- preparsed Inline type
+  | T Text -- preparsed Text type
 
 {-
 	Examples
@@ -36,61 +43,66 @@ data Token = Star               -- used for bold/italic
 
 -- a demo for nested lists
 doc1 :: Document
-doc1 = [
-    Heading1 [Normal "This is my first header"],
+doc1 =
+  [ Heading1 [Normal "This is my first header"],
     Heading2 [Normal "This tests", Bold "bold", Normal "text."],
     Heading2 [Normal "This tests", Italic "italic", Normal "text."],
     Heading2 [Normal "This tests", BoldItalic "bold and italic", Normal "text."],
     Heading2 [Normal "This tests", Preformatted "preformatted", Normal "text."],
-    Paragraph [
-        Normal "This is not a normal paragraph. I can make things",
-        Bold "bold", Normal "or", Italic "italic", Normal "or",
-        BoldItalic "both", Normal ". I can even have", Preformatted "preformatted",
+    Paragraph
+      [ Normal "This is not a normal paragraph. I can make things",
+        Bold "bold",
+        Normal "or",
+        Italic "italic",
+        Normal "or",
+        BoldItalic "both",
+        Normal ". I can even have",
+        Preformatted "preformatted",
         Normal "text. Isn't markdown cool?!"
-        ]
-    ]
+      ]
+  ]
 
 -- a demo for ordered lists
 doc2 :: Document
-doc2 = [
-    Heading1 [Normal "The Top 3 Programming Languages"],
-    OL [
-        Paragraph [Normal "Python"],
+doc2 =
+  [ Heading1 [Normal "The Top 3 Programming Languages"],
+    OL
+      [ Paragraph [Normal "Python"],
         Paragraph [Normal "JavaScript"],
         Paragraph [Normal "GoLang"]
-        ]
-    ]
+      ]
+  ]
 
 -- a demo for nested lists
 doc3 :: Document
-doc3 = [
-    Heading1 [Normal "The families of programming languages"],
-    UL [
-        LI [
-            Paragraph [Normal "Machine Code"],
-            OL [
-                LI [Paragraph [Normal "CISC"]],
+doc3 =
+  [ Heading1 [Normal "The families of programming languages"],
+    UL
+      [ LI
+          [ Paragraph [Normal "Machine Code"],
+            OL
+              [ LI [Paragraph [Normal "CISC"]],
                 LI [Paragraph [Normal "RISC"]]
-                ]
-            ],
-        LI [
-            Paragraph [Normal "Assembly"],
-            OL [
-                LI [Paragraph [Normal "ARM"]],
+              ]
+          ],
+        LI
+          [ Paragraph [Normal "Assembly"],
+            OL
+              [ LI [Paragraph [Normal "ARM"]],
                 LI [Paragraph [Normal "X86"]],
                 LI [Paragraph [Normal "6502"]]
-                ]
-            ],
-        LI [
-            Paragraph [Normal "High Level"],
-            OL [
-                LI [Paragraph [Normal "C"]],
+              ]
+          ],
+        LI
+          [ Paragraph [Normal "High Level"],
+            OL
+              [ LI [Paragraph [Normal "C"]],
                 LI [Paragraph [Normal "Haskell"]],
                 LI [Paragraph [Normal "JavaScript"]]
-                ]
-            ]
-        ]
-    ]
+              ]
+          ]
+      ]
+  ]
 
 {-
 	Function Definitions
@@ -98,10 +110,10 @@ doc3 = [
 
 -- this gets the file names from the user
 getFiles :: [String] -> (String, String)
-getFiles []       = error "Error: No files provided."       -- no files
-getFiles [i]      = error "Error: No output file provided." -- one file
-getFiles [i,o]    = (i, o)                                  -- two files
-getFiles (i:o:xs) = error "Error: Too many files provided." -- N files
+getFiles [] = error "Error: No files provided." -- no files
+getFiles [i] = error "Error: No output file provided." -- one file
+getFiles [i, o] = (i, o) -- two files
+getFiles (i : o : xs) = error "Error: Too many files provided." -- N files
 
 --lexer :: String -> [Token]
 --parser :: [Tokens] -> [Block]
@@ -110,13 +122,13 @@ getFiles (i:o:xs) = error "Error: Too many files provided." -- N files
 -- this is the backbone holding up the other functions
 main :: IO ()
 main = do
-    args <- getArgs                       -- get system args
-    let (infile, outfile) = getFiles args -- get file names
-    putStrLn ("Input: " ++ infile)        -- show input file name
-    putStrLn ("Output: " ++ outfile)      -- show output file name
---  let mdText = read infile
---  let lexed = lexer mdText
---  let parsed = parser lexed
---  let html = printHTML parsed
---  write html outfile
-    print "OK."                           -- inform user that program is done
+  args <- getArgs -- get system args
+  let (infile, outfile) = getFiles args -- get file names
+  putStrLn ("Input: " ++ infile) -- show input file name
+  putStrLn ("Output: " ++ outfile) -- show output file name
+  --  let mdText = read infile
+  --  let lexed = lexer mdText
+  --  let parsed = parser lexed
+  --  let html = printHTML parsed
+  --  write html outfile
+  print "OK." -- inform user that program is done
