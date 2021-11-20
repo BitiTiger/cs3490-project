d828c17d78e6f35d9a1f0942a9e979d1d7b39a15
Cameron Himes <cameron.himes@hotmail.com>
2021-11-20 18:14:28 -0500

    add some examples

--- project/converter.hs
+++ project/converter.hs
@@ -12,9 +12,13 @@ type Text = String
 data Inline = Normal Text       -- normal text
             | Bold Text         -- bold text
             | Italic Text       -- italic text
+            | BoldItalic Text   -- bold and italic text
             | Preformatted Text -- inline code
-data Block = UL [Block]         -- Unordered List
+data Block = LI [Block]         -- List Items
+           | UL [Block]         -- Unordered List
            | OL [Block]         -- Ordered List
+           | Heading1 [Inline]  -- Heading 1
+           | Heading2 [Inline]  -- Heading 2
            | Paragraph [Inline] -- Paragraph
            | Code [Inline]      -- Code block
 type Document = [Block]
@@ -25,6 +29,68 @@ data Token = Star               -- used for bold/italic
            | B Block            -- preparsed Block type
            | I Inline           -- preparsed Inline type
            | T Text             -- preparsed Text type
+
+{-
+	Examples
+-}
+
+-- a demo for nested lists
+doc1 :: Document
+doc1 = [
+    Heading1 [Normal "This is my first header"],
+    Heading2 [Normal "This tests", Bold "bold", Normal "text."],
+    Heading2 [Normal "This tests", Italic "italic", Normal "text."],
+    Heading2 [Normal "This tests", BoldItalic "bold and italic", Normal "text."],
+    Heading2 [Normal "This tests", Preformatted "preformatted", Normal "text."],
+    Paragraph [
+        Normal "This is not a normal paragraph. I can make things",
+        Bold "bold", Normal "or", Italic "italic", Normal "or",
+        BoldItalic "both", Normal ". I can even have", Preformatted "preformatted",
+        Normal "text. Isn't markdown cool?!"
+        ]
+    ]
+
+-- a demo for ordered lists
+doc2 :: Document
+doc2 = [
+    Heading1 [Normal "The Top 3 Programming Languages"],
+    OL [
+        Paragraph [Normal "Python"],
+        Paragraph [Normal "JavaScript"],
+        Paragraph [Normal "GoLang"]
+        ]
+    ]
+
+-- a demo for nested lists
+doc3 :: Document
+doc3 = [
+    Heading1 [Normal "The families of programming languages"],
+    UL [
+        LI [
+            Paragraph [Normal "Machine Code"],
+            OL [
+                LI [Paragraph [Normal "CISC"]],
+                LI [Paragraph [Normal "RISC"]]
+                ]
+            ],
+        LI [
+            Paragraph [Normal "Assembly"],
+            OL [
+                LI [Paragraph [Normal "ARM"]],
+                LI [Paragraph [Normal "X86"]],
+                LI [Paragraph [Normal "6502"]]
+                ]
+            ],
+        LI [
+            Paragraph [Normal "High Level"],
+            OL [
+                LI [Paragraph [Normal "C"]],
+                LI [Paragraph [Normal "Haskell"]],
+                LI [Paragraph [Normal "JavaScript"]]
+                ]
+            ]
+        ]
+    ]
 
 {-
 	Function Definitions
